6a92edf823ddfc49a83be3fa407576142ad71d63
sungmin <lsm1511@naver.com>
2022-07-28 11:16:08 +0900

    phone쇼핑몰홈페이지

--- js_work_nodejs2022/server.js
+++ js_work_nodejs2022/server.js
@@ -14,11 +14,12 @@ app.use(cors());
 // __dirname = 전체 경로(C:\WORK_SUNGMIN\js_work_nodejs2022)
 app.use(express.static(__dirname + '/public'));
 
+let top = 1;
 let phoneList = [
-    {pno:1, name:'iphone10', price:3500, company: 'APPLE', year:2020},
-    {pno:2, name:'galaxy', price:3500, company: 'SAMSUNG', year:2020},
-    {pno:3, name:'iphone11', price:3500, company: 'APPLE', year:2021},
-    {pno:4, name:'wide3', price:3500, company: 'SAMSUNG', year:2022}
+    {pno:top++, name:'iphone10', price:3500, company: 'APPLE', year:2020},
+    {pno:top++, name:'galaxy', price:3500, company: 'SAMSUNG', year:2020},
+    {pno:top++, name:'iphone11', price:3500, company: 'APPLE', year:2021},
+    {pno:top++, name:'wide3', price:3500, company: 'SAMSUNG', year:2022}
 ];
 
 // 라우트(route) 설정
@@ -29,12 +30,45 @@ app.get('/', (req, res)=> {
 
 
 app.get('/home', (req, res)=> {
-    req.app.render('home', {phoneList},(err, data)=>{
+    req.app.render('home', {phoneList},(err, htmlData)=>{
         if(err) throw err;
         res.end(htmlData);
     });
 });
 
+// ejs 페이지로 forword
+app.get('/input', (req, res)=> {
+    req.app.render('input', {phoneList},(err, htmlData)=>{
+        if(err) throw err;
+        res.end(htmlData);
+    });
+});
+// input 데이터 처리
+app.get('/input_ok', (req, res)=> {
+    let phoneData = {
+        pno : top++,
+        name : req.query.name,
+        price : req.query.price,
+        company : req.query.company,
+        year : req.query.year
+    };
+    phoneList.push(phoneData);
+    res.redirect('/home');
+});
+
+app.get('/detail', (req, res)=> {
+    req.app.render('detail', {phoneList},(err, htmlData)=>{
+        if(err) throw err;
+        res.end(htmlData);
+    });
+});
+
+app.get('/modify', (req, res)=> {
+    req.app.render('modify', {phoneList},(err, htmlData)=>{
+        if(err) throw err;
+        res.end(htmlData);
+    });
+});
 
 const server = http.createServer(app);
 server.listen(app.get('port'), ()=>{
