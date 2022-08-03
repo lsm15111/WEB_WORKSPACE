dfb621e605271faad610edc60737cb3d1a5bf513
sungmin <lsm1511@naver.com>
2022-08-03 17:50:22 +0900

    Main

--- react_workspace2022/day01/my-react-app/src/App.js
+++ react_workspace2022/day01/my-react-app/src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 import { render } from '@testing-library/react';
-import TodoList from './TodoList';
 import TodoList2 from './TodoList2';
-import InputWork from './InputWork';
+
+// render 함수형,클래스형
+//컴포넌트에 생성 - class에서 return , html태그에 생성 - ReactDOM.render
 
 // 1. 클래스형 컴포넌트
 // 2. 함수형 컴포넌트
 // 3. 외부 파일에 컴포넌트 선언
 function App() {
+  
+  
   return (
     <div>
       <h1>Hello LSM</h1>
