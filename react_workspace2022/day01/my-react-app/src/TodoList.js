327f92b85e6158ff69bc614d0aadeb0f6e608170
sungmin <lsm1511@naver.com>
2022-08-03 15:54:45 +0900

    TodoList 크래스 파일

---
+++ react_workspace2022/day01/my-react-app/src/TodoList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+class TodoList extends React.Component{
+    constructor(props){
+        console.log(props.title);
+        super(props);
+        this.state=[];
+        this.state={
+            data: "hahaha"
+        };
+    }
+    
+    
+    render(){
+        let styleObj= {color:"blue",textDecoration:"underline"};
+        return (<>
+        <h2>Todo List</h2>
+        <div>
+        <label htmlFor="">할일 : </label>
+        <input type="text" value={this.state.data} onChange={(e)=>{
+            this.setState({data: e.currentTarget.value})
+        }}/>
+        <div id="status">상태: {this.state.data}</div>
+        <ol>
+            <li>
+                <span style={styleObj}>프로젝트 기획하기</span>
+                <button>완료</button>
+                <button>삭제</button>
+            </li>
+        </ol>
+        </div>
+        </>);
+    }
+}
+
+export default TodoList;
