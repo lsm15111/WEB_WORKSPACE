14b9fe6de0bf247844c602cc9faaa89a8925563e
sungmin <lsm1511@naver.com>
2022-08-03 16:33:38 +0900

    Main

--- react_workspace2022/day01/my-react-app/src/App.js
+++ react_workspace2022/day01/my-react-app/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import { render } from '@testing-library/react';
+import TodoList from './TodoList';
+import TodoList2 from './TodoList2';
+import InputWork from './InputWork';
 
+// 1. 클래스형 컴포넌트
+// 2. 함수형 컴포넌트
+// 3. 외부 파일에 컴포넌트 선언
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello LSM</h1>
+      <TodoList2 title="할 일 목록"/>
     </div>
   );
 }
-
 export default App;
