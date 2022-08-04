e205dbfbf17ad43f5d6fc1d3e75a9ff87075f0bf
sungmin <lsm1511@naver.com>
2022-08-04 11:56:17 +0900

    Main

--- react_workspace2022/react-app-day02ex01/src/App.js
+++ react_workspace2022/react-app-day02ex01/src/App.js
@@ -1,33 +1,42 @@
+import { useState } from 'react';
+import './App.css';
+import MovieInput from './MovieInput'
+import MovieSearch from './MovieSearch'
+import MovieList from './MovieList'
+
 
 
 function App() {
+  const today = new Date().toISOString().substring(0,10);
+  const [movieList,setMovieList] =useState([
+    {no:1,movietitle:"토르", openDate:"2022-06-28",rating:4},
+    {no:2,movietitle:"마녀2", openDate:"2022-07-10",rating:5},
+    {no:3,movietitle:"탑건", openDate:"2022-07-15",rating:5}
+  ]);
+  const [searchList,setSearchList] = useState(movieList)
+  function saveMovieFn(movietitle,openDate){
+    let mno = movieList.length + 1;
+    let newData = {no:mno,movietitle:movietitle, openDate:openDate,rating:5}
+    let newList = [...movieList, newData]; // (...) = 기존의 리스트를 복제해옴
+    setMovieList(newList);
+  }
+
+  function searchMovieFn(keyword){
+    let newList = movieList.filter(function(movie){
+      return movie.movieTitle.indexOf(keyword) !== 1;
+    });
+    setSearchList(newList);
+  }
+
   return (<>
     <div>
       <h1>영화 평점 정보 관리</h1>
+      <h3>오늘날짜: {today}</h3>
     </div>
-    <div>
-      {/* input form*/}
-      <fieldset >
-        <legend>영화 정보 입력</legend>
-        <label>영화제목</label>
-        <input type="text" value={movieTitle} onChange={(e)=>{
-          setMovieTitle(e.target.value);
-        }}></input>
-      </fieldset>
-      
-    </div>
-    <div>
-      {/* search form*/}
-      <fieldset>
-        <legend>영화 정보 검색</legend>
-      </fieldset>
-    </div>
-    <div>
-      {/* list form*/}
-      <fieldset>
-        <legend>영화 정보 목록</legend>
-      </fieldset>
-    </div>
+    <MovieInput onSaveMovie={saveMovieFn}/>
+    <MovieSearch onSearchMovie={searchMovieFn}/>
+    <MovieList movieList={movieList} setMovieList={setMovieList}/>
+    
   </>);
 }
 
