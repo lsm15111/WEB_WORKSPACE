37e67141283598b20e343e3606f1ee8aa4c8ec3b
sungmin <lsm1511@naver.com>
2022-08-04 15:37:53 +0900

    react-redux (Provider) main

--- react_workspace2022/react-app-day02ex02_redux/src/index.js
+++ react_workspace2022/react-app-day02ex02_redux/src/index.js
@@ -4,10 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './App';
 
-const currentState = {}
+const currentState = {체중:70, 키:170}
 
 function reducer(state = currentState, action){
-
+if(action.type === '체중증가'){
+  state.체중++;
+}
+if(action.type === '키증가'){
+  state.키++;
+}
+const newState = {...state};
+return newState;
 }
 
 let store = createStore(reducer);
