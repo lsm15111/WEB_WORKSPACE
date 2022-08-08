852c1eedc4482e92c82701be38f26cdde1886036
sungmin <lsm1511@naver.com>
2022-08-08 14:06:23 +0900

    firebase React Ajax lecture

--- react_ajax_firebase_lecture/src/components/Router.js
+++ react_ajax_firebase_lecture/src/components/Router.js
@@ -1,13 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 import Profile from "../routes/Profile";
 
-const AppRouter = (props) => {
+const AppRouter = ({isLoggedIn, onLogin, onLogout}) => {
+    
     return (<BrowserRouter>
         <Routes>
-            <Route path="/profile" element={<Profile></Profile>} />
-            <Route path="*" element={<Home></Home>} />
+            <Route path="/" element={
+                isLoggedIn ? (
+                    <Home onLogout={onLogout}></Home>
+                ) : (
+                    <Auth onLogin={onLogin}></Auth>
+                )} />
         </Routes>
     </BrowserRouter>)
 }
--- react_ajax_firebase_lecture/src/firebase.conf.js
+++ react_ajax_firebase_lecture/src/firebase.conf.js
@@ -1,5 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/storage";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCRaOcA5avW8ZRFfp_OD5shSz0Y660yH9U",
@@ -10,5 +12,8 @@ const firebaseConfig = {
   appId: "1:1014465985818:web:c5b775e555e3774cfa80a3",
   measurementId: "G-8NFX1K8VTD"
 };
+firebase.initializeApp(firebaseConfig);
 
-export default firebase.initializeApp(firebaseConfig);
+export const authService = firebase.auth();
+export const dbService = firebase.firestore();
+export const storageService = firebase.storage();
